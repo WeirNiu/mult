a1712cf49d47b041152c749fe972f312e52f5536
Weirdo <525340603@qq.com>
2018-08-08 21:35:40 +0800

    add springjunit4 baseTest

---
+++ src/test/java/com.mult/BaseJunit4Test.java
@@ -0,0 +1,30 @@
+package com.mult;
+
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.mock.jndi.SimpleNamingContextBuilder;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class) //使用junit4进行测试
+@ContextConfiguration(locations={"classpath:spring-mvc.xml", "classpath*:spring-mybatis.xml" }) //加载配置文件
+//------------如果加入以下代码，所有继承该类的测试类都会遵循该配置，也可以不加，在测试类的方法上///控制事务，参见下一个实例
+//这个非常关键，如果不加入这个注解配置，事务控制就会完全失效！
+//@Transactional
+//这里的事务关联到配置文件中的事务控制器（transactionManager = "transactionManager"），同时//指定自动回滚（defaultRollback = true）。这样做操作的数据才不会污染数据库！
+//@TransactionConfiguration(transactionManager = "transactionManager", defaultRollback = true)
+//------------
+public class BaseJunit4Test {
+    @BeforeClass
+    public static void beforeClass() throws Exception {
+        ClassPathXmlApplicationContext ac = new ClassPathXmlApplicationContext(
+                "classpath:jndi.xml");
+        DriverManagerDataSource ds = (DriverManagerDataSource) ac.getBean("dataSource");
+        SimpleNamingContextBuilder builder = new SimpleNamingContextBuilder();
+        builder.bind("java:comp/env/mult", ds);
+        builder.activate();
+    }
+
+}
