df92a35f2fb95889b9110618df3d7674cc010659
Weirniu <525340603@qq.com>
2018-08-13 23:01:09 +0800

    update migrate

--- src/main/java/com/mult/basic/init/MigrationSqlite.java
+++ src/main/java/com/mult/basic/init/MigrationSqlite.java
@@ -22,14 +22,13 @@ public class MigrationSqlite {
         Flyway flyway = new Flyway();
         //设置加载数据库的相关配置信息
         flyway.setDataSource(dataSource);
-        // 设置接受flyway进行版本管理的多个数据库
+        //设置接受flyway进行版本管理的多个数据库setSchemas("")
+        //设置存放flyway metadata数据的表名，默认"schema_version"，可不写;
         flyway.setTable("schema_version");
         //设置flyway扫描sql升级脚本、java升级脚本的目录路径或包路径，默认"db/migration"，可不写
         flyway.setLocations("db");
         //设置sql脚本文件的编码，默认"UTF-8"
         flyway.setEncoding("UTF-8");
-        //flyway.setSchemas("mult");
-        //设置存放flyway metadata数据的表名，默认"schema_version"，可不写
         flyway.setBaselineOnMigrate(true);
         flyway.migrate();
     }
