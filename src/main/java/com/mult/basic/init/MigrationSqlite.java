43074e79dad5ee5daa03b8a92a3b31217101a72c
Weirdo <525340603@qq.com>
2018-08-16 13:37:45 +0800

    update flyway initMethod

--- src/main/java/com/mult/basic/init/MigrationSqlite.java
+++ src/main/java/com/mult/basic/init/MigrationSqlite.java
@@ -1,29 +1,42 @@
 package com.mult.basic.init;
 
+import org.apache.commons.dbcp.BasicDataSource;
 import org.flywaydb.core.Flyway;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.util.Properties;
 
 /**
  * @author Weirdo
  * 项目启动时同步数据库结构
  */
+@Component
 public class MigrationSqlite {
 
-    private DataSource dataSource;
+    @Value("#{jdbc}")
+    Properties jdbc;
 
-    public void setDataSource(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public DataSource getDataSource(){
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(jdbc.getProperty("jdbc.driver"));
+        ds.setUrl(jdbc.getProperty("jdbc.url"));
+        ds.setUsername(jdbc.getProperty("jdbc.username"));
+        ds.setPassword(jdbc.getProperty("jdbc.password"));
+        return ds;
     }
 
     /**
      * 数据库结构迁移入口
      */
+    @PostConstruct
     public void migrate() {
         //初始化flyway类
         Flyway flyway = new Flyway();
         //设置加载数据库的相关配置信息
-        flyway.setDataSource(dataSource);
+        flyway.setDataSource(getDataSource());
         //设置接受flyway进行版本管理的多个数据库setSchemas("")
         //设置存放flyway metadata数据的表名，默认"schema_version"，可不写;
         flyway.setTable("schema_version");
